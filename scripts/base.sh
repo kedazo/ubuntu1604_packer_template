aecda118a40c01a4581d39463ab0320b5a5c3503
David Kedves <david.kedves@borgcollective.eu>
2016-04-22 14:22:32 +0200

    small updates

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-perl -p -i -e 's#http://us.archive.ubuntu.com/ubuntu#http://mirror.rackspace.com/ubuntu#gi' /etc/apt/sources.list
+#perl -p -i -e 's#http://us.archive.ubuntu.com/ubuntu#http://mirror.rackspace.com/ubuntu#gi' /etc/apt/sources.list
+
+# update source list
+cat <<EOF > /etc/apt/sources.list
+deb http://hu.archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse
+# deb-src http://hu.archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse
+
+deb http://hu.archive.ubuntu.com/ubuntu/ xenial-updates main restricted universe multiverse
+# deb-src http://hu.archive.ubuntu.com/ubuntu/ xenial-updates main restricted universe multiverse
+
+deb http://hu.archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse
+# deb-src http://hu.archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse
+
+deb http://security.ubuntu.com/ubuntu xenial-security main restricted universe multiverse
+# deb-src http://security.ubuntu.com/ubuntu xenial-security main restricted universe multiverse
+
+# deb http://hu.archive.ubuntu.com/ubuntu/ xenial-proposed multiverse main restricted universe
+# deb-src http://hu.archive.ubuntu.com/ubuntu/ xenial-proposed main restricted universe multiverse
+EOF
 
 # Update the box
-apt-get -y update >/dev/null
-apt-get -y install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
+apt-get -y -q update
+apt-get -y -q install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip
 
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config
--- scripts/dep.sh
+++ scripts/dep.sh
@@ -6,5 +6,6 @@ apt-get -y -q update
 apt-get -y -q install curl wget || true
 apt-get -y -q install gnuplot-nox gdb || true
 apt-get -y -q  --force-yes dist-upgrade
+apt-get clean
 
 # You can install anything you need here.
