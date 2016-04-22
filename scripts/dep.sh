770f222362c01332f681997aab1b99a5b7e1ddca
David Kedves <david.kedves@borgcollective.eu>
2016-04-22 13:44:17 +0200

    Updated ISO URLs to point to Ubuntu 16.04 LTS image + small updates

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -2,8 +2,9 @@
 #
 # Setup the the box. This runs as root
 
-apt-get -y update
-
-apt-get -y install curl
+apt-get -y -q update
+apt-get -y -q install curl wget || true
+apt-get -y -q install gnuplot-nox gdb || true
+apt-get -y -q  --force-yes dist-upgrade
 
 # You can install anything you need here.
--- scripts/zerodisk.sh
+++ scripts/zerodisk.sh
@@ -2,6 +2,7 @@
 
 # Zero out the free space to save space in the final image:
 dd if=/dev/zero of=/EMPTY bs=1M
+sync
 rm -f /EMPTY
 
 # Sync to ensure that the delete completes before this moves on.
