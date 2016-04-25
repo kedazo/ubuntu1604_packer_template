468a6ffa4b8c7c8805ef2dbf96209d9ff120961e
David Kedves <david.kedves@borgcollective.eu>
2016-04-25 09:53:38 +0200

    temporary *hack* as facter is not getting installed (not available?)

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Bail if we are not running inside VirtualBox.
-if [[ `facter virtual` != "virtualbox" ]]; then
-    exit 0
-fi
+#if [[ `facter virtual` != "virtualbox" ]]; then
+#    echo "Not virtualbox, or 'facter' is not installed"
+#    exit 0
+#fi
+
+echo "Running VirtualBox additions installer..."
 
 mkdir -p /mnt/virtualbox
 mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
